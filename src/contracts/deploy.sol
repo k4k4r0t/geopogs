b67cca14c78645384c6794df8c58d4a014239597
k4k4r0t <kakarot4real@protonmail.com>
2021-02-20 23:22:23 -0800

    to the earth!

---
+++ src/contracts/deploy.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.0;
+
+import "./geopogs.sol";
+
+/**
+ * @dev This is an example contract implementation of NFToken.
+ */
+contract GeoPogsDeploy is
+  GeoPogs("GeoPogs", "GPOG", "https://geopogs.party/pogs/")
+{
+
+}
